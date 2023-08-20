52a7197a3c8c1137d59aca750568852c16dffe82
Gokul_Gangadharan_Coforge <gokulbornforsuccess@gmail.com>
2023-08-20 18:46:29 +0530

    c2

--- src/Core_Java_Concepts/Encapsulation.java
+++ src/Core_Java_Concepts/Encapsulation.java
@@ -1,4 +1,19 @@
 package Core_Java_Concepts;
 
-public class Encapsulation {
+public class Encapsulation extends X{
+    public static void main(String[] args) {
+        Encapsulation g=new Encapsulation();
+        g.setter(10);
+        System.out.println(g.getter());
+    }
+
+}
+class X{
+    private int x=10;
+    public int getter(){
+        return x;
+    }
+    public void setter(int x){
+        this.x=x;
+    }
 }
