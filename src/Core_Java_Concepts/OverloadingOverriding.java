14e69549a51d145090cd3fede6f679e3c45f1d10
Gokul_Gangadharan_Coforge <gokulbornforsuccess@gmail.com>
2023-08-20 19:05:27 +0530

    Method overloading

--- src/Core_Java_Concepts/OverloadingOverriding.java
+++ src/Core_Java_Concepts/OverloadingOverriding.java
@@ -1,4 +1,21 @@
 package Core_Java_Concepts;
 
 public class OverloadingOverriding {
+    static int add(int i1,int i2){
+        return i1+i2;
+    }
+    static float add(float i1, float i2){
+        return i1+i2;
+    }
+    public static void main(String[] args) {
+        System.out.println(add(10,11));
+        System.out.println(add(11.5F,12.5F));
+
+    }
+}
+class override{
+    static int sum(int i1,int i2){
+        System.out.println("Method inside override Class");
+        return i1+i2;
+    }
 }
